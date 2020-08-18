46167d988ae52205bc61f4cc96ac1bee459cd4b0
BastienUnder <sebastienfranks@gmail.com>
2020-08-18 17:51:02 -0600

    Again to sqlite

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
         #Postgres
         #'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -88,12 +88,12 @@ DATABASES = {
         #'PORT': '',
 
         #Mysql
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'first_blog',
-        'USER': 'sebastien',
-        'PASSWORD': '123',
-        'HOST': '',
-        'PORT': '',
+        #'ENGINE': 'django.db.backends.oracle',
+       # 'NAME': 'first_blog',
+        #'USER': 'sebastien',
+        #'PASSWORD': '123',
+        #'HOST': '',
+        #'PORT': '',
     }
 }
 
