4f7a3dbf4c3a74d459925f8b77ab33e66a2baae9
BastienUnder <sebastienfranks@gmail.com>
2020-08-19 14:04:08 -0600

    Trying postgres now that we added comment model

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,16 +76,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
         #Postgres
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': 'first_django_app',
-        #'USER': 'sebastien',
-        #'PASSWORD': '123',
-        #'HOST': 'localhost',
-        #'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'first_django_app',
+        'USER': 'sebastien',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '',
 
         #Mysql
         #'ENGINE': 'django.db.backends.oracle',
