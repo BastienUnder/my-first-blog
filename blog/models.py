0cbf6c0fb66a635c947374d0e6e94ff89bb2ffcd
BastienUnder <sebastienfranks@gmail.com>
2020-08-18 18:34:00 -0600

    SQlite and migrate on your app specifically

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+#rando test comment
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
